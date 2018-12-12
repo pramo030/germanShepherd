5ca977326131c3230d6a12f01ef2090006471052
Pedro Ramos <pramo030@fiu.edu>
2018-12-12 11:56:06 -0500

    creation of some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { MaleComponent } from './male/male.component';
+import { FemaleComponent } from './female/female.component';
+import { ContactComponent } from './contact/contact.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { PuppiesComponent } from './puppies/puppies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    HomeComponent,
+    AboutusComponent,
+    MaleComponent,
+    FemaleComponent,
+    ContactComponent,
+    ComingSoonComponent,
+    PuppiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/puppies/puppies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-puppies',
+  templateUrl: './puppies.component.html',
+  styleUrls: ['./puppies.component.css']
+})
+export class PuppiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
